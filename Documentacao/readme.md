9686170cad35c9884e37d371a18d00ba790ffc3d
Paulo <paulodeoliveirasantos68@gmail.com>
2025-12-19 16:30:44 -0300

    Primeiro Commit: Importando a pasta do Projeto Ponto

---
+++ Documentacao/readme.md
@@ -0,0 +1,13 @@
+# Ideia de etapas para o projeto:
+
+## Primeira fase:
+### 1. Orquestração do Banco de dados:
+Utilizei o XAMPP para criar um banco de dados Mysql, chamado sistema_ponto, e nele, criei uma tabela "usuários", que contem 4 colunas, são essas: id, nome, email, senha. Essas colunas receberão os dados de usuário.
+
+### 2. Criação dos arquivos para conexão e configuração:
+Feito a criação de arquivos para a configuração com o banco, no momento os arquivos ainda estão sendo configurados.
+- Arquivo conexao.php: estrutura a conexao com o banco, realiza uma amostra na tela, evidenciando se a conexão foi ou não bem sucedida.
+- login.php: Ponte de conexão entre o front e o banco.
+
+### 3. Criação do Front:
+O front utilizará HTML, CSS e JavaScript. Será uma composição básica, mas que para iniciação do projeto será suficiente, e possibilitará upgrades futuros.
---
+++ criar_usuario.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/erro.log');
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/conexao.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    error_log("POST RECEBIDO: " . print_r($_POST, true));
+
+    $nome   = $_POST['nome_usuario'] ?? null;
+    $email  = $_POST['email_usuario'] ?? null;
+    $perfil = $_POST['perfil_usuario'] ?? null;
+    $senhaHash = password_hash($_POST['senha_usuario'], PASSWORD_DEFAULT);
+
+    try {
+        $sql = "INSERT INTO usuarios (nome, email, senha, perfil)
+                VALUES (:nome, :email, :senha, :perfil)";
+
+        error_log("SQL: $sql");
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':senha', $senhaHash);
+        $stmt->bindParam(':perfil', $perfil);
+
+        $stmt->execute();
+
+        echo "Usuário cadastrado com sucesso!";
+
+    } catch (PDOException $e) {
+        error_log("ERRO PDO: " . $e->getMessage());
+        echo "Erro ao cadastrar usuário. Verifique o log.";
+    }
+}
