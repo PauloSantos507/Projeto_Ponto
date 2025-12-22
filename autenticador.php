a80684b79a7a3a8abaef67c54be1ce09c636bb86
Paulo <paulodeoliveirasantos68@gmail.com>
2025-12-22 15:42:00 -0300

    Adicionei arquivo login

---
+++ autenticador.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $email = $_POST['email_login'];
+    $senha = $_POST['senha_login'];
+
+    try {
+        $sql = "SELECT id, nome, senha FROM usuarios WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $usuario = $stmt->fetch();
+
+        //Verifica se o usuário que está tentando acessar existe, e se sua senha bate com a senha que está no banco
+        if ($usuario && password_verify($senha, $usuario['senha'])) {
+            $_SESSION['usuario_id'] = $usuario['ids'];
+            $_SESSION['usuario_nome'] = $usuario['nome'];
+
+            header("Location: bater_ponto.php");
+            exit(); // É uma boa prática adicionar exit() após um redirecionamento de cabeçalho
+        } else {
+            echo "<script>alert('E-mail ou senha inválidos!'); window.location.href='login.html';</script>";
+        }
+    } catch (PDOException $e) {
+        echo "Erro no sistema: " . $e->getMessage();
+    }
+}
